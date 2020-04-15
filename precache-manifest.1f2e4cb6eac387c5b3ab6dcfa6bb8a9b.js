908b379fd634f2e873547a4c7f981d4dc0314e5a
Risto Holappa <risto_holappa@hotmail.com>
2020-04-15 12:01:01 +0300

    move development to develop branch help on deploy on free account

---
+++ precache-manifest.1f2e4cb6eac387c5b3ab6dcfa6bb8a9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c25fe62ec7ab856a1eec8aff5e68a7",
+    "url": "/rholappa.github.io/index.html"
+  },
+  {
+    "revision": "d10de1da657bfbb70c2b",
+    "url": "/rholappa.github.io/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "ceef19ada15c5f3ef6da",
+    "url": "/rholappa.github.io/static/js/2.648dbb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rholappa.github.io/static/js/2.648dbb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10de1da657bfbb70c2b",
+    "url": "/rholappa.github.io/static/js/main.5e1b77ce.chunk.js"
+  },
+  {
+    "revision": "73037b983e10ac7cf9f3",
+    "url": "/rholappa.github.io/static/js/runtime-main.9ee02137.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/rholappa.github.io/static/media/logo.ee7cd8ed.svg"
+  }
+]);
