4bc92fd1dde61cd62401a09cc09731c668757f5f
Risto Holappa <risto_holappa@hotmail.com>
2020-04-15 06:34:04 +0300

    Updates

---
+++ precache-manifest.d65261a682c45428774d206d1c4f655a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2a35c10d3a6908b055b0173a27a431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e728b9cc49562cb54742",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ceef19ada15c5f3ef6da",
+    "url": "/static/js/2.648dbb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.648dbb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e728b9cc49562cb54742",
+    "url": "/static/js/main.dd83f12c.chunk.js"
+  },
+  {
+    "revision": "f166c8cf10a00d4ec2b5",
+    "url": "/static/js/runtime-main.933c6c27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
