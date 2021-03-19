bfa46c810ef8b85f7d070473b3b9ad48bb43e095
Risto Holappa <risto_holappa@hotmail.com>
2021-03-19 20:34:28 +0200

    testing pwa deployment

---
+++ dist/server/3.js
@@ -0,0 +1,16 @@
+exports.ids = [3];
+exports.modules = {
+
+/***/ 27:
+/***/ (function(module, exports) {
+
+module.exports = { 
+      attributes: {"title":"Info","link":"guides/info","cover":"./killing-zombie-with-axes.svg","description":"","date":"June 26, 2019","products":null},
+    
+      html: "",
+     }
+
+/***/ })
+
+};;
+//# sourceMappingURL=3.js.map
